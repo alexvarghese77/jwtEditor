a987c8c1badf5ed91beeec923f7d86f723ac5c41
Abhijit Kurup <abhijit@keyvalue.systems>
2022-08-27 10:50:27 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <div className="navBar">
+        <span className="header">JWT Editor</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
